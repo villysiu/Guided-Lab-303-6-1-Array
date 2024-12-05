bb62b8f3725657c93b6625e2ceb6b7a7ab25a7cb
Villy Siu <villysiu@gmail.com>
2024-12-05 14:36:41 -0800

    added

--- src/EnhancedForLoop.java
+++ src/EnhancedForLoop.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] names = { "New York", "Dallas", "Las Vegas", "Florida" };
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
 }
